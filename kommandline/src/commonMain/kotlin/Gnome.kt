735e500c749164daae3228a1545f875d2a64213c
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-10-08 11:53:27 +0200

    experiments

--- kommandline/src/commonMain/kotlin/Gnome.kt
+++ kommandline/src/commonMain/kotlin/Gnome.kt
@@ -94,7 +94,6 @@ data class GnomeTerm(
 // for example to be able to clear notifications in optimal ways in comparison to:
 // dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.panel.statusArea.dateMenu._messageList._sectionList.get_children().forEach(s => s.clear());'
 // TODO: also: analyze and implement Kommands for stuff like:
-// dbus-run-session -- gnome-shell --nested --wayland
 // xgettext --output=locale/example.pot *.js (https://www.codeproject.com/Articles/5271677/How-to-Create-A-GNOME-Extension)
 // msginit --locale fr --input locale/example.pot --output
 // msgfmt example.po --output-file=example.mo
