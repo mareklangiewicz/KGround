e651c81c4b811db75023acdb34fb34db04a9c29d
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-10-08 12:45:57 +0200

    experiments

---
+++ kommandline/src/commonMain/kotlin/DConfEditor.kt
@@ -0,0 +1,24 @@
+package pl.mareklangiewicz.kommand
+
+import pl.mareklangiewicz.kommand.DConfEditor.Option
+
+/** [dconf-editor ubuntu manpage](http://manpages.ubuntu.com/manpages/impish/man1/dconf-editor.1.html) */
+fun dconfedit(vararg options: Option, init: DConfEditor.() -> Unit = {}) =
+    DConfEditor(options.toMutableList()).apply(init)
+
+/** [dconf-editor ubuntu manpage](http://manpages.ubuntu.com/manpages/impish/man1/dconf-editor.1.html) */
+data class DConfEditor(
+    val options: MutableList<Option> = mutableListOf(),
+    val nonopts: MutableList<String> = mutableListOf(),
+): Kommand {
+    override val name get() = "dconf-editor"
+    override val args get() = options.map { it.str } + nonopts
+    sealed class Option(val str: String) {
+        object help : Option("--help")
+        object version : Option("--version")
+        object relocatableschemas : Option("--list-relocatable-schemas")
+        object skipwarning : Option("--I-understand-that-changing-options-can-break-applications")
+    }
+    operator fun Option.unaryMinus() = options.add(this)
+    operator fun String.unaryPlus() = nonopts.add(this)
+}
