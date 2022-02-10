7a41b402bf5f681d773d5c46330e4bd2b495f9fb
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-02-10 13:38:13 +0100

    fun

--- kommandline/src/commonTest/kotlin/demos/LangaraREPL.kt
+++ kommandline/src/commonTest/kotlin/demos/LangaraREPL.kt
@@ -5,6 +5,9 @@ package pl.mareklangiewicz.kommand.demos
 import pl.mareklangiewicz.kommand.*
 import pl.mareklangiewicz.kommand.coreutils.*
 
+/** Changes in this fun should NOT be pushed to GIT. */
 fun Platform.LangaraREPL() {
+
     lsRegFiles("/home/marek").printlns()
+
 }
