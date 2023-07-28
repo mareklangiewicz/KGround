898e08f7df9b12738f363d2a0d4eaf5d284eb6e1
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-07-28 17:07:19 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultBuildTemplateForRootProject(
         name = "KommandLine",
         description = "Kotlin DSL for popular CLI commands.",
         githubUrl = "https://github.com/langara/KommandLine",
-        version = Ver(0, 0, 18)
+        version = Ver(0, 0, 19)
     ),
     withSonatypeOssPublishing = true,
 )
