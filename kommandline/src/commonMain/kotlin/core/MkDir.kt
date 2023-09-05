2923e1eba12047357089c79fedf45b85fc5e1803
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-09-05 22:18:22 +0200

    fun

--- kommandline/src/commonMain/kotlin/core/MkDir.kt
+++ kommandline/src/commonMain/kotlin/core/MkDir.kt
@@ -1,41 +1,41 @@
 package pl.mareklangiewicz.kommand.core
 
 import pl.mareklangiewicz.kommand.*
-import pl.mareklangiewicz.kommand.core.MkdirOpt.*
+import pl.mareklangiewicz.kommand.core.MkDirOpt.*
 
 @OptIn(DelicateKommandApi::class)
 fun mkdir(dir: String, withParents: Boolean = false) =
     mkdir { if (withParents) -Parents; +dir }.reducedOutToUnit()
 
 @DelicateKommandApi
-fun mkdir(init: Mkdir.() -> Unit = {}) = Mkdir().apply(init)
+fun mkdir(init: MkDir.() -> Unit = {}) = MkDir().apply(init)
 
 /**
  * [linux man](https://man7.org/linux/man-pages/man1/mkdir.1.html)
  * [gnu coreutils mkdir manual](https://www.gnu.org/software/coreutils/manual/html_node/mkdir-invocation.html#mkdir-invocation)
  */
 @DelicateKommandApi
-data class Mkdir(
-    override val opts: MutableList<MkdirOpt> = mutableListOf(),
+data class MkDir(
+    override val opts: MutableList<MkDirOpt> = mutableListOf(),
     override val nonopts: MutableList<String> = mutableListOf(),
-) : KommandTypical<MkdirOpt> { override val name get() = "mkdir" }
+) : KommandTypical<MkDirOpt> { override val name get() = "mkdir" }
 
 @DelicateKommandApi
-interface MkdirOpt: KOptTypical {
+interface MkDirOpt: KOptTypical {
 
     /** set file mode (as in chmod), not a=rwx - umask */
-    data class Mode(val mode: String): KOptS("m", mode), MkdirOpt
+    data class Mode(val mode: String): KOptS("m", mode), MkDirOpt
 
     /**
      * no error if existing, make parent directories as needed,
      * with their file modes unaffected by any -m option.
      */
-    data object Parents : KOptS("p"), MkdirOpt
+    data object Parents : KOptS("p"), MkDirOpt
 
     /** print a message for each created directory */
-    data object Verbose : KOptS("v"), MkdirOpt
+    data object Verbose : KOptS("v"), MkDirOpt
 
-    data object Help : KOptL("help"), MkdirOpt
+    data object Help : KOptL("help"), MkDirOpt
 
-    data object Version : KOptL("version"), MkdirOpt
+    data object Version : KOptL("version"), MkDirOpt
 }
