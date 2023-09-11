8c164d1689a4b50e7cbec5a261f1c874b7593e35
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-09-11 15:32:30 +0200

    fun

--- kommandline/src/commonMain/kotlin/core/Touch.kt
+++ kommandline/src/commonMain/kotlin/core/Touch.kt
@@ -4,7 +4,7 @@ import pl.mareklangiewicz.kommand.*
 
 /** Update the access and modification times of each file to the current time. Create empty files if necessary. */
 @OptIn(DelicateKommandApi::class)
-fun touch(vararg files: String) = touch { this.files.addAll(files) }.reducedOutToUnit()
+fun touch(vararg files: String) = touch { nonopts.addAll(files) }.reducedOutToUnit()
 
 @DelicateKommandApi
 fun touch(init: Touch.() -> Unit) = Touch().apply(init)
@@ -12,23 +12,18 @@ fun touch(init: Touch.() -> Unit) = Touch().apply(init)
 /** [linux man](https://man7.org/linux/man-pages/man1/touch.1.html) */
 @DelicateKommandApi
 data class Touch(
-    val options: MutableList<Option> = mutableListOf(),
-    val files: MutableList<String> = mutableListOf()
-) : Kommand {
+    override val opts: MutableList<TouchOpt> = mutableListOf(),
+    override val nonopts: MutableList<String> = mutableListOf()
+) : KommandTypical<TouchOpt> { override val name get() = "touch" }
 
-    override val name get() = "touch"
-    override val args get() = options.map { it.str } + files
 
-    sealed class Option(val str: String) {
-        data object timeOfAccessOnly : Option("-a")
-        data object timeOfChangeOnly : Option("-m")
-        data object disableCreation : Option("-c")
-        data class date(val date: String): Option("--date=$date")
-        data object help : Option("--help")
-        data object version : Option("--version")
-    }
 
-    operator fun String.unaryPlus() = files.add(this)
-
-    operator fun Option.unaryMinus() = options.add(this)
+@DelicateKommandApi
+interface TouchOpt: KOptTypical {
+    data object TimeOfAccessOnly : TouchOpt, KOptS("a")
+    data object TimeOfChangeOnly : TouchOpt, KOptS("m")
+    data object DisableCreation : TouchOpt, KOptS("c")
+    data class Date(val date: String): TouchOpt, KOptL("date", date)
+    data object Help : TouchOpt, KOptL("help")
+    data object Version : TouchOpt, KOptL("version")
 }
