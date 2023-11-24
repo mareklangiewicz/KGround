f9133f812acfc6780fc00b92bc6b1b059421af72
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-11-24 14:22:17 +0100

    fun

--- kgroundx-maintenance/src/jvmTest/kotlin/maintenance/MaintenanceTests.kt
+++ kgroundx-maintenance/src/jvmTest/kotlin/maintenance/MaintenanceTests.kt
@@ -17,19 +17,19 @@ class MaintenanceTests {
     // KGround$ ./gradlew test --tests MaintenanceTests.maintenanceTestFactory
     @TestFactory
     fun maintenanceTestFactory() = uspekTestFactory {
-//        "check all known regions synced" o { checkAllKnownRegionsSynced(verbose = true) }
-//        "check all known regions in my kotlin projects" o { runBlocking { checkAllKnownRegionsInMyProjects() } }
-//        "check my dworkflows in my projects" o { runBlocking { checkMyDWorkflowsInMyProjects(onlyPublic = true) } }
-//
-//        "DANGEROUS inject all known regions to sync" o { injectAllKnownRegionsToSync() }
-//        "DANGEROUS inject all known regions to all my projects" o { runBlocking { injectAllKnownRegionsToMyProjects() } }
-//        "DANGEROUS inject dworkflows to all my projects" o { runBlocking { injectMyDWorkflowsToMyProjects(onlyPublic = true) } }
-//        "DANGEROUS inject dworkflows to Some Proj" o { injectDWorkflowsToKotlinProject("KommandLine") }
-//
-//        "DANGEROUS updateKGroundResourcesSymLinks" o { updateKGroundResourcesSymLinks() }
-//        "DANGEROUS updateGradlewFilesInMyProjects" o { runBlocking { updateGradlewFilesInMyProjects(onlyPublic = false) } }
-//
-//        "DANGEROUS inject hacky workflow to refreshDeps repo" o { injectHackyGenerateDepsWorkflowToRefreshDepsRepo() }
-//        "DANGEROUS inject updateGeneratedDeps workflow to DepsKt repo" o { injectUpdateGeneratedDepsWorkflowToDepsKtRepo() }
+       // "check all known regions synced" o { checkAllKnownRegionsSynced(verbose = true) }
+       // "check all known regions in my kotlin projects" o { runBlocking { checkAllKnownRegionsInMyProjects() } }
+       // "check my dworkflows in my projects" o { runBlocking { checkMyDWorkflowsInMyProjects(onlyPublic = true) } }
+
+       // "DANGEROUS inject all known regions to sync" o { injectAllKnownRegionsToSync() }
+       // "DANGEROUS inject all known regions to all my projects" o { runBlocking { injectAllKnownRegionsToMyProjects() } }
+       // "DANGEROUS inject dworkflows to all my projects" o { runBlocking { injectMyDWorkflowsToMyProjects(onlyPublic = true) } }
+       // "DANGEROUS inject dworkflows to Some Proj" o { injectDWorkflowsToKotlinProject("KommandLine") }
+
+       // "DANGEROUS updateKGroundResourcesSymLinks" o { updateKGroundResourcesSymLinks() }
+       // "DANGEROUS updateGradlewFilesInMyProjects" o { runBlocking { updateGradlewFilesInMyProjects(onlyPublic = false) } }
+
+       // "DANGEROUS inject hacky workflow to refreshDeps repo" o { injectHackyGenerateDepsWorkflowToRefreshDepsRepo() }
+       // "DANGEROUS inject updateGeneratedDeps workflow to DepsKt repo" o { injectUpdateGeneratedDepsWorkflowToDepsKtRepo() }
     }
 }
