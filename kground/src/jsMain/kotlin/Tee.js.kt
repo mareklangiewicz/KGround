ba10941472b8f13baaf9780851ed415ae208aa79
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-01-21 13:49:11 +0100

    fun

--- kground/src/jsMain/kotlin/Tee.js.kt
+++ kground/src/jsMain/kotlin/Tee.js.kt
@@ -1,6 +1,7 @@
 package pl.mareklangiewicz.kground
 
 import kotlinx.browser.*
+import org.w3c.dom.Window
 import kotlin.js.*
 
 actual fun getCurrentTimeMs(): Long = Date().getMilliseconds().toLong()
@@ -13,7 +14,11 @@ actual fun getCurrentTimeStr(): String = getCurrentTimeMs().toString()
 actual fun getCurrentThreadName(): String = "JS Thread"
 
 
-actual fun getCurrentPlatformName(): String = window.navigator.userAgent.let { "JS Agent $it" }
+actual fun getCurrentPlatformName(): String =
+    windowOrNull?.navigator?.userAgent?.let { "JS Agent $it" } ?: "Probably Node.js"
 
 // FIXME_someday: real path on node.js platform? something more appropriate in browser too? rename? write kdoc?
-actual fun getCurrentAbsolutePath(): String = window.document.location.toString()
+actual fun getCurrentAbsolutePath(): String =
+    windowOrNull?.document?.location?.toString() ?: js("process.cwd()")
+
+val windowOrNull: Window? get() = if (js("typeof window !== 'undefined'")) window else null
