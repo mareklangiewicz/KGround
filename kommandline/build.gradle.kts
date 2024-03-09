c952f069c2e06a50cbc36df0ecccdc03cc237f9e
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-03-09 12:28:32 +0100

    fun

--- kommandline/build.gradle.kts
+++ kommandline/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 defaultBuildTemplateForBasicMppLib {
-    api("pl.mareklangiewicz:kground:0.0.39")
+    api("pl.mareklangiewicz:kground:0.0.41")
     // FIXME https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/kground/
 }
 
