f6484cd66be87c0bf00cbb388b9fc489da182a52
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-04-07 18:50:05 +0200

    fun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,37 +1,37 @@
 @file:Suppress("UnstableApiUsage")
 
-//gradle.logSomeEventsToFile(rootProject.projectDir.toOkioPath() / "my.gradle.log")
+// gradle.logSomeEventsToFile(rootProject.projectDir.toOkioPath() / "my.gradle.log")
 
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
 
-    val depsKtDir = File("../DepsKt")
+  val depsKtDir = File("../DepsKt")
 
-   val depsKtLocal = depsKtDir.exists()
-    // val depsKtLocal = false
+  val depsKtLocal = depsKtDir.exists()
+  // val depsKtLocal = false
 
-    if (depsKtLocal) {
-        logger.warn("Including local build $depsKtDir")
-        includeBuild(depsKtDir)
-    }
+  if (depsKtLocal) {
+    logger.warn("Including local build $depsKtDir")
+    includeBuild(depsKtDir)
+  }
 }
 
 plugins {
-    id("pl.mareklangiewicz.deps.settings") version "0.2.93" // https://plugins.gradle.org/search?term=mareklangiewicz
-    id("com.gradle.enterprise") version "3.16.2" // https://docs.gradle.com/enterprise/gradle-plugin/
+  id("pl.mareklangiewicz.deps.settings") version "0.2.94" // https://plugins.gradle.org/search?term=mareklangiewicz
+  id("com.gradle.enterprise") version "3.16.2" // https://docs.gradle.com/enterprise/gradle-plugin/
 }
 
 gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
-        publishOnFailure()
-    }
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+    publishOnFailure()
+  }
 }
 
 rootProject.name = "KommandLine"
@@ -46,7 +46,7 @@ val kgroundLocal = kgroundDir.exists()
 // val kgroundLocal = false
 
 if (kgroundLocal) {
-    logger.warn("Adding local kground module.")
-    include(":kground")
-    project(":kground").projectDir = kgroundDir
+  logger.warn("Adding local kground module.")
+  include(":kground")
+  project(":kground").projectDir = kgroundDir
 }
