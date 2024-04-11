a549ddc46e059f63be532abcf3f799039e71da77
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-04-11 11:19:18 +0200

    fun

--- kgroundx-io/build.gradle.kts
+++ kgroundx-io/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 defaultBuildTemplateForBasicMppLib {
   api(project(":kgroundx"))
   api(project(":kground-io"))
-  api(findProject(":kommandline") ?: Langiewicz.kommandline.withVer("0.0.50"))
-  api(findProject(":kommandsamples") ?: Langiewicz.kommandsamples.withVer("0.0.50"))
+  api(findProject(":kommandline") ?: Langiewicz.kommandline.withVer("0.0.51"))
+  api(findProject(":kommandsamples") ?: Langiewicz.kommandsamples.withVer("0.0.51"))
   // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/kommandline/
 }
 
