95a3d888aa165f35a3b19b1c067d630c09e71d5d
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-05-30 14:53:03 +0200

    fun

--- kgroundx-io/build.gradle.kts
+++ kgroundx-io/build.gradle.kts
@@ -17,8 +17,8 @@ defaultBuildTemplateForBasicMppLib {
 
 setMyWeirdSubstitutions(
   // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/kommandline/
-  "kommandline" to "0.0.61",
-  "kommandsamples" to "0.0.61",
+  "kommandline" to "0.0.62",
+  "kommandsamples" to "0.0.62",
   "kground" to "ALWAYS_LOCAL", // to avoid issues with trans deps from kommandline
 )
 
