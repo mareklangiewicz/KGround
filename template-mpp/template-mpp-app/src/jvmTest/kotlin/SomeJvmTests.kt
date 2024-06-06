e81fbce5f39141aa02f47c631c228980e5858ae1
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-06 11:43:09 +0200

    fun

--- template-mpp/template-mpp-app/src/jvmTest/kotlin/SomeJvmTests.kt
+++ template-mpp/template-mpp-app/src/jvmTest/kotlin/SomeJvmTests.kt
@@ -2,14 +2,16 @@ import org.junit.jupiter.api.TestFactory
 import pl.mareklangiewicz.templatempp.*
 import pl.mareklangiewicz.uspek.*
 
+internal infix fun <T> T.chkEq(expected: T) = check(this == expected) { "$this != $expected" }
+
 class SomeJvmTests {
 
   @TestFactory fun mainCliJvmTest() = uspekTestFactory {
     "On template cli-like project" o {
-      "Check helloCommon output" o { helloCommon() eq "Hello Pure Common World!" }
-      "Check helloCommon output - should fail" ox { helloCommon() eq "Incorrect output" }
-      "Check helloPlatform output" o { helloPlatform().startsWith("Hello JVM World") eq true }
-      "Check helloPlatform output - should fail" ox { helloPlatform() eq "Incorrect output" }
+      "Check helloCommon output" o { helloCommon() chkEq "Hello Pure Common World!" }
+      "Check helloCommon output - should fail" ox { helloCommon() chkEq "Incorrect output" }
+      "Check helloPlatform output" o { helloPlatform().startsWith("Hello JVM World") chkEq true }
+      "Check helloPlatform output - should fail" ox { helloPlatform() chkEq "Incorrect output" }
       "Just run whole mainCli fun" o { mainCli() }
     }
   }
