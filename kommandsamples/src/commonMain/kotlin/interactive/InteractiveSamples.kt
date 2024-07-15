b02b9287dd3c9a3d9dce4d640ddc12ef1adf8dec
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-07-15 14:03:59 +0200

    fun

--- kommandsamples/src/commonMain/kotlin/interactive/InteractiveSamples.kt
+++ kommandsamples/src/commonMain/kotlin/interactive/InteractiveSamples.kt
@@ -16,12 +16,14 @@ import pl.mareklangiewicz.kommand.lineRawOrNull
 import pl.mareklangiewicz.kommand.samples.*
 import pl.mareklangiewicz.kommand.writeFileWithDD
 import pl.mareklangiewicz.kommand.xclipOut
-import pl.mareklangiewicz.kommand.zenity.zenityAskIf
 import pl.mareklangiewicz.ulog.ULog
 import pl.mareklangiewicz.ulog.i
 import pl.mareklangiewicz.ulog.implictx
 import pl.mareklangiewicz.ure.*
 import pl.mareklangiewicz.ureflect.getReflectCallOrNull
+import pl.mareklangiewicz.usubmit.USubmit
+import pl.mareklangiewicz.usubmit.implictx
+import pl.mareklangiewicz.usubmit.xd.askIf
 
 
 /**
@@ -29,7 +31,7 @@ import pl.mareklangiewicz.ureflect.getReflectCallOrNull
  *   For example, "pl.mareklangiewicz.kommand.demo.MyDemoSamples#getBtop"
  */
 @NotPortableApi
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 suspend fun tryInteractivelySomethingRef(reference: String = "xclip") {
   val log = implictx<ULog>()
   log.i("tryInteractivelySomethingRef(\"$reference\")")
@@ -53,7 +55,7 @@ suspend fun tryInteractivelySomethingRef(reference: String = "xclip") {
 }
 
 @NotPortableApi
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 suspend fun tryInteractivelyClassMember(className: String, memberName: String) {
   val log = implictx<ULog>()
   log.i("tryInteractivelyClassMember(\"$className\", \"$memberName\")")
@@ -62,7 +64,8 @@ suspend fun tryInteractivelyClassMember(className: String, memberName: String) {
   // before we start to interact with the user,
   // but the code is never called without confirmation.
   ifInteractiveCodeEnabled {
-    zenityAskIf("Call member $memberName\nfrom class $className?").ax() || return
+    val submit = implictx<USubmit>()
+    submit.askIf("Call member $memberName\nfrom class $className?") || return
     val member: Any? = call()
     // Note: call() will either already "do the thing" (when the member is just a fun to call)
     //  or it will only get the property (like ReducedScript/Sample etc.) which will be tried (or not) later.
@@ -71,7 +74,7 @@ suspend fun tryInteractivelyClassMember(className: String, memberName: String) {
 }
 
 
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 suspend fun Any?.tryInteractivelyAnything() = when (this) {
   is Sample -> tryInteractivelyCheckSample()
   is Kommand -> toInteractiveCheck().ax()
@@ -81,36 +84,38 @@ suspend fun Any?.tryInteractivelyAnything() = when (this) {
 }
 
 
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 suspend fun Sample.tryInteractivelyCheckSample() =
   kommand.toInteractiveCheck(expectedLineRaw).ax()
 
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 suspend fun ReducedSample<*>.tryInteractivelyCheckReducedSample() {
   reducedKommand.lineRawOrNull() chkEq expectedLineRaw // so also if both are nulls it's treated as fine.
   tryInteractivelyCheckReducedScript("Exec ReducedSample ?")
 }
 
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 suspend fun ReducedScript<*>.tryInteractivelyCheckReducedScript(
   question: String = "Exec ReducedScript ?",
 ) {
-  zenityAskIf(question).ax() || return
+  val submit = implictx<USubmit>()
+  submit.askIf(question) || return
   val reducedOut = ax()
   reducedOut.tryOpenDataInIDE("Open ReducedOut: ${reducedOut.about} in tmp.notes in IDE ?")
 }
 
-@DelicateApi("API for manual interactive experimentation. Requires Zenity, conditionally skips")
+@DelicateApi("API for manual interactive experimentation. Conditionally skips")
 /** @param question null means default question */
 suspend fun Any?.tryOpenDataInIDE(question: String? = null): Any {
   val log = implictx<ULog>()
   val fs = implictx<UFileSys>()
+  val submit = implictx<USubmit>()
   return when {
     this == null -> log.i("It is null. Nothing to open.")
     this is Unit -> log.i("It is Unit. Nothing to open.")
     this is String && isEmpty() -> log.i("It is empty string. Nothing to open.")
     this is Collection<*> && isEmpty() -> log.i("It is empty collection. Nothing to open.")
-    !zenityAskIf(question ?: "Open $about in tmp.notes in IDE ?").ax() -> log.i("Not opening.")
+    !submit.askIf(question ?: "Open $about in tmp.notes in IDE ?") -> log.i("Not opening.")
     else -> {
       val lines = if (this is Collection<*>) map { it.toString() } else toString().lines()
       val notes = fs.pathToTmpNotes.toString() // FIXME_later: use Path type everywhere
