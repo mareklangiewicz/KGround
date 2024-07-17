20eb87e40b96b824d920eb4a8f8d2fafa3fb28a1
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-07-17 10:36:20 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ defaultBuildTemplateForRootProject(
     name = "KGround",
     description = "Kotlin Common Ground.",
     githubUrl = "https://github.com/mareklangiewicz/KGround",
-    version = Ver(0, 0, 67),
+    version = Ver(0, 0, 68),
     // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/kground/
     // https://github.com/mareklangiewicz/KGround/releases
     settings = LibSettings(
