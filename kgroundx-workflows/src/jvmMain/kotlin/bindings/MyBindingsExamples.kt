eed2a81dcf8cae914abfd92a11dc921e8609b51b
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-11-11 10:25:34 +0100

    fun

--- kgroundx-workflows/src/jvmMain/kotlin/bindings/MyBindingsExamples.kt
+++ kgroundx-workflows/src/jvmMain/kotlin/bindings/MyBindingsExamples.kt
@@ -16,6 +16,9 @@ object MyBindingsExamples {
     ActionCoords("EndBug", "add-and-commit", "v9"),
     ActionCoords("gradle", "actions/setup-gradle", "v4"),
     ActionCoords("gradle", "actions/dependency-submission", "v4"),
+    ActionCoords("actions", "upload-artifact", "v4"),
+    ActionCoords("actions", "download-artifact", "v4"),
+    ActionCoords("softprops", "action-gh-release", "v2"),
   ).forEach { coords ->
     val outputDir = PProjKGround / "kgroundx-workflows/src/jvmMain/kotlin/bindings/generated"
     coords.tryGenerateBindingsToDir(NewestForVersion, outputDir)
