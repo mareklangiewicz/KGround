9173646ace8ad66fd1a51e92fff56f8a6048624d
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-03-24 22:54:31 +0100

    fun

--- kgroundx-jupyter/src/jvmMain/kotlin/Integration.kt
+++ kgroundx-jupyter/src/jvmMain/kotlin/Integration.kt
@@ -1,12 +1,19 @@
 package pl.mareklangiewicz.kgroundx.jupyter
 
-import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterIntegration
+// import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterIntegration
+//
+// internal class Integration : JupyterIntegration() {
+//   override fun Builder.onLoaded() {
+//     import("kotlinx.coroutines.*")
+//     import("kotlinx.coroutines.flow.*")
+//     import("pl.mareklangiewicz.kground.*")
+//   }
+// }
 
-internal class Integration : JupyterIntegration() {
-  override fun Builder.onLoaded() {
-    import("kotlinx.coroutines.*")
-    import("kotlinx.coroutines.flow.*")
-    import("pl.mareklangiewicz.kground.*")
+internal class Integration {
+  init {
+      TODO("Temporarily broken because of issues with vanniktech publish plugin")
+      // See also comment in build.gradle.kts
   }
 }
 
