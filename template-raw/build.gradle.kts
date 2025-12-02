37c10a67fcdf9fa83a3a63827360a620159c9073
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-12-02 12:25:37 +0100

    fun

--- template-raw/build.gradle.kts
+++ template-raw/build.gradle.kts
@@ -9,7 +9,12 @@ plugins {
   plug(plugs.KotlinMulti) apply false
   plug(plugs.KotlinJvm) apply false
   plug(plugs.KotlinMultiCompose) apply false
-  plug(plugs.ComposeJb) apply false // ComposeJbEdge can be very slow to sync, clean, build (jb dev repo issue)
+
+  // plug(plugs.ComposeJb) apply false // ComposeJbEdge can be very slow to sync, clean, build (jb dev repo issue)
+  id("org.jetbrains.compose") version "1.10.0-beta02"
+  // TODO_later: Check again after compose update, because now default version fails with:
+  // Cannot determine the version of Skiko for Compose '1.10.0-rc01'
+
   plug(plugs.AndroKmp) apply false
   plug(plugs.AndroApp) apply false
   plug(plugs.VannikPublish) apply false
@@ -28,11 +33,13 @@ defaultBuildTemplateForRootProject(
     name = "TemplateRaw",
     description = "Raw template for multi platform projects.",
     githubUrl = "https://github.com/mareklangiewicz/KGround/tree/main/template-raw",
-    version = Ver(0, 0, 33),
+    version = Ver(0, 0, 34),
     settings = LibSettings(
       withJs = enableJs,
       withLinuxX64 = enableLinux,
       withKotlinxHtml = true, // also used in common code
+      withTestJUnit5 = true,
+      withTestJUnit4OnAndroidDevice = true,
       compose = LibComposeSettings(
         withComposeHtmlCore = enableJs,
         withComposeHtmlSvg = enableJs,
