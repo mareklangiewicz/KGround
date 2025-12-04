c19f0fea40ca38220ae9ab8cf411d418c53bb43d
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-12-04 15:09:18 +0100

    fun

--- template-raw/template-raw-lib/src/androidDeviceTest/kotlin/MyDeviceTest.kt
+++ template-raw/template-raw-lib/src/androidDeviceTest/kotlin/MyDeviceTest.kt
@@ -1,41 +1,45 @@
 package com.example.template_raw_lib
 
-import android.os.Build
-import android.util.Log
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
-import org.junit.Test
-import org.junit.runner.RunWith
+import android.os.*
+import android.util.*
+import androidx.test.ext.junit.runners.*
+import androidx.test.platform.app.*
+import org.junit.*
+import org.junit.Assert.*
+import org.junit.runner.*
 
-private const val TAG = "DeviceTestInfo"
+/**
+ * The "tee" like util, to log any value using Log.i with custom "teeI" tag
+ * @return The same provided value, so it can be chained/injected in expressions as debug tool.
+ * BTW filter logcat by tag to quickly find these logs.
+ */
+private val <T> T.teeI get(): T = apply { Log.i("teeI", this.toString()) }
 
 @RunWith(AndroidJUnit4::class)
 class MyDeviceTest {
 
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("pl.mareklangiewicz.templateraw.test", appContext.packageName)
-    }
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("pl.mareklangiewicz.templateraw.test", appContext.packageName)
+  }
 
-    @Test
-    fun logDetailedDeviceInfo() {
-        // Example of logging detailed device information for easy filtering in Logcat.
-        Log.i(TAG, "Manufacturer: ${Build.MANUFACTURER}")
-        Log.i(TAG, "Model: ${Build.MODEL}")
-        Log.i(TAG, "Brand: ${Build.BRAND}")
-        Log.i(TAG, "Device: ${Build.DEVICE}")
-        Log.i(TAG, "Product: ${Build.PRODUCT}")
-        Log.i(TAG, "Hardware: ${Build.HARDWARE}")
-        Log.i(TAG, "Board: ${Build.BOARD}")
-        Log.i(TAG, "SDK Level: ${Build.VERSION.SDK_INT}")
-        Log.i(TAG, "Release Version: ${Build.VERSION.RELEASE}")
-        Log.i(TAG, "Development Codename: ${Build.VERSION.CODENAME}")
-        Build.SUPPORTED_ABIS.forEachIndexed { index, abi ->
-            Log.i(TAG, "ABI ${index + 1}: $abi")
-        }
+  @Test
+  fun logDetailedDeviceInfo() {
+    // Example of logging detailed device information for easy filtering in Logcat.
+    "Manufacturer: ${Build.MANUFACTURER}".teeI
+    "Model: ${Build.MODEL}".teeI
+    "Brand: ${Build.BRAND}".teeI
+    "Device: ${Build.DEVICE}".teeI
+    "Product: ${Build.PRODUCT}".teeI
+    "Hardware: ${Build.HARDWARE}".teeI
+    "Board: ${Build.BOARD}".teeI
+    "SDK Level: ${Build.VERSION.SDK_INT}".teeI
+    "Release Version: ${Build.VERSION.RELEASE}".teeI
+    "Development Codename: ${Build.VERSION.CODENAME}".teeI
+    Build.SUPPORTED_ABIS.forEachIndexed { index, abi ->
+      "ABI ${index + 1}: $abi".teeI
     }
+  }
 }
