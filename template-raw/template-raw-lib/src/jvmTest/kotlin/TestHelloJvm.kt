dd4ed9d2974608a364bd30ad1519d623b0d0f18b
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-12-06 15:07:56 +0100

    fun

--- template-raw/template-raw-lib/src/jvmTest/kotlin/TestHelloJvm.kt
+++ template-raw/template-raw-lib/src/jvmTest/kotlin/TestHelloJvm.kt
@@ -1,10 +1,27 @@
 import org.junit.jupiter.api.TestFactory
 import pl.mareklangiewicz.uspek.*
 
+/**
+ * The "tee" like util, to log any value to stdout.
+ * @return The same provided value, so it can be chained/injected in expressions as debug tool.
+ */
+private val <T> T.tee: T get() = apply { println(this.toString()) }
+
 class TestHelloJvm {
 
   @TestFactory
   fun testHelloJvm() = uspekTestFactory {
-    "On testHelloJvm" o { onHelloStuff() }
+    "On testHelloJvm" o {
+      "On logDetailedJvmInfo" o {
+        "OS Name: ${System.getProperty("os.name")}".tee
+        "OS Version: ${System.getProperty("os.version")}".tee
+        "OS Arch: ${System.getProperty("os.arch")}".tee
+        "Java Version: ${System.getProperty("java.version")}".tee
+        "Java Vendor: ${System.getProperty("java.vendor")}".tee
+        "JVM Name: ${System.getProperty("java.vm.name")}".tee
+        "User Directory: ${System.getProperty("user.dir")}".tee
+      }
+      onHelloStuff()
+    }
   }
 }
