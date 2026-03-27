bf0a4f5b103b4a6400bc911269ae49176792dc03
Marek Langiewicz <marek.langiewicz@gmail.com>
2026-03-27 10:17:23 +0100

    fun

--- template-full/template-full-app/build.gradle.kts
+++ template-full/template-full-app/build.gradle.kts
@@ -227,7 +227,7 @@ fun KotlinMultiplatformExtension.allDefault(
   }
   if (withJvm) jvm()
   if (withJs) jsDefault()
-  if (withNativeLinux64) linuxX64()
+  if (withLinuxX64) linuxX64()
   if (withAndro && !ignoreAndroTarget) androidTarget {
     // TODO_someday some kmp andro publishing. See kdoc above why not yet.
   }
@@ -263,7 +263,7 @@ fun KotlinMultiplatformExtension.allDefault(
         }
       }
     }
-    if (withNativeLinux64) {
+    if (withLinuxX64) {
       val linuxX64Main by getting
       val linuxX64Test by getting
     }
@@ -328,7 +328,7 @@ fun Project.defaultBuildTemplateForBasicMppApp(
     if (details.settings.withJs) js(IR) {
       binaries.executable()
     }
-    if (details.settings.withNativeLinux64) linuxX64 {
+    if (details.settings.withLinuxX64) linuxX64 {
       binaries {
         executable {
           entryPoint = details.run { "$appMainPackage.$appMainFun" }
@@ -453,7 +453,7 @@ fun Project.defaultBuildTemplateForComposeMppApp(
     if (details.settings.withJs) js(IR) {
       binaries.executable()
     }
-    if (details.settings.withNativeLinux64) linuxX64 {
+    if (details.settings.withLinuxX64) linuxX64 {
       binaries {
         executable {
           entryPoint = "${details.appMainPackage}.${details.appMainFun}"
